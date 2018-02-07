7d86e4fbff42f7e0564b4529372185f5254f27ba
HVKHVK <vkasikaralar@gmail.com>
2018-02-07 18:36:10 +0300

    GRB

--- wind.py
+++ wind.py
@@ -9,6 +9,6 @@ def main():
     #latitude = input("Please enter latitude: ")
     #longitude = input("Please enter longitude: ")
 	#cmd_input = 'cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv'
- 	subprocess.call("cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 "+ grb_file + ".grb -csv " + grb_file + ".csv",shell=True)
+ 	subprocess.call("cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv",shell=True)
 
 main()
