fae24196aefdc531ef94db0533970fefe8863e07
HVKHVK <vkasikaralar@gmail.com>
2018-02-07 18:15:54 +0300

    GRB

--- wind.py
+++ wind.py
@@ -8,14 +8,17 @@ def main():
     #grb_file = input("Please enter name of grb file (ex:2018020418_f000):")
     #latitude = input("Please enter latitude: ")
     #longitude = input("Please enter longitude: ")
-	cmd_input = 'cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv'
+	#cmd_input = 'cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv'
 
-	command = ['cd']
+	#command = ['cd']
 	#command.append('/usr/src/GRB/;') 
 	#command.append('/usr/bin/grib2/wgrib2/wgrib2')
 	#command.append('2018020418_f000.grb')
 	#command.append('-csv')
 	#command.append('2018020418_f000.csv')
-    (out,err) =subprocess.Popen(command,stdout=subprocess.PIPE).communicate()
+    #(out,err) =subprocess.Popen(command,stdout=subprocess.PIPE).communicate()
+    command = ['date']
+	command.append('+%Y-%m-%d %H:%M:%S')
+	(out,err) = subprocess.Popen(command,stdout=subprocess.PIPE).communicate()
 
 main()
