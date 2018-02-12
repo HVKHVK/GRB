73f61198e9a052a283d9ac68f0e7e4446e59e3f2
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 13:52:42 +0300

    GRB

--- wind.py
+++ wind.py
@@ -5,13 +5,13 @@ import subprocess, shlex
 
 
 def main():
-    #filename = sys.argv[1]
-    #lan = sys.argv[2]
-    #lon = sys.argv[3]
-    #meter = sys.argv[4]
-    command_line = input()
-    args = shlex.split(command_line)
-    print(args);
-    p = subprocess.Popen(args)
+    filename = sys.argv[1]
+    lan = sys.argv[2]
+    lon = sys.argv[3]
+    meter = sys.argv[4]
+    #command_line = input()
+    #args = shlex.split(command_line)
+    #print(args);
+    p = subprocess.Popen(['cd','usr/src/GRB;', '/usr/bin/grib2/wgrib2/wgrib2', filename, '-s', '-lon', lan, lon ])
 
 main()
