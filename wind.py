e97636f640eb26dbfe6ce5495ca04ab85292153f
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 14:31:17 +0300

    GRB

--- wind.py
+++ wind.py
@@ -11,6 +11,6 @@ def main():
     lon = sys.argv[3]
     meter = sys.argv[4]
     p = subprocess.Popen(['/usr/bin/grib2/wgrib2/wgrib2', filename, '-s', '-lon', lan, lon ], stdout=subprocess.PIPE)
-    print(p.stdout)
+    print(p)
 
 main()
