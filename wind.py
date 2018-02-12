4321fb54ed23faa2711771a8f031ee19b6de6ae4
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 16:31:02 +0300

    GRB

--- wind.py
+++ wind.py
@@ -28,15 +28,15 @@ def main():
             if ('UGRD' == output[x+13:x+17]):
                 meter.append(output[x+18:x+21])
                 if('val=' == output[x+69:x+73]):
-                    print(output[x+73:x+82])
+                    UGRD.append(output[x+73:x+82])
                 elif ('val=' == output[x+70:x+74]):
-                    print(output[x+74:x+83])
+                    UGRD.append(output[x+74:x+83])
             elif ('VGRD' == output[x+13:x+17]):
                 meter.append(output[x+18:x+21])
                 if('val=' == output[x+69:x+73]):
-                    print(output[x+73:x+82])
+                    VGRD.append(output[x+73:x+82])
                 elif ('val=' == output[x+70:x+74]):
-                    print(output[x+74:x+83])
+                    VGRD.append(output[x+74:x+83])
 
 # print(date[0],date[1],date[2],date[3])
 #   print(meter[0],meter[1],meter[2],meter[3])
