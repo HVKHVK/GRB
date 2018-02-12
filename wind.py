e1a67b5ea2f74e22280c90091ed7a8452539d82d
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 13:24:55 +0300

    GRB

--- wind.py
+++ wind.py
@@ -7,7 +7,9 @@ import subprocess
 def enter():
 	
 def main():
-   	grb_file = input("Please enter name of grb file (ex:2018020418_f000): ")
-	subprocess.call("cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv" ,shell=True)
+    text = input("prompt")
+    print (text)
+    #grb_file = input("Please enter name of grb file (ex:2018020418_f000): ")
+    #subprocess.call("cd /usr/src/GRB/; /usr/bin/grib2/wgrib2/wgrib2 2018020418_f000.grb -csv 2018020418_f000.csv" ,shell=True)
 
 main()
