3fc5411df0fc1ffe94bb6008bb916306ab7c6202
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 15:46:38 +0300

    GRB

--- wind.py
+++ wind.py
@@ -12,18 +12,28 @@ def main():
     p = subprocess.Popen(['/usr/bin/grib2/wgrib2/wgrib2', filename, '-s', '-lon', lan, lon ], stdout=subprocess.PIPE)
     output = p.stdout.read().decode("utf-8")
     length = len(output)
-
+    
+    coordinates = lan + ' ' + lon
+    
     date = []
-    coordinates = []
     meter = []
     speed = []
     direction = []
+    UGRD = []
+    VGRD = []
 
     for x in range(1,length):
         if('d=' == output[x:x+2]):
             date2 = output[x+8:x+10] + '/' + output[x+6:x+8] + '/' + output[x+2:x+6] + ' ' + output[x+10:x+12] + ':00'
             date.append(date2)
-            #print (date[0],)
+            if ('UGRD' == output[x+14:x+17]):
+                UGRD = output[x+19:x:21]
+            elif ('VGRD' == output[x+14:x+17]):
+                VGRD = output[x+19:x:21]
 
-    print (date[0],date[1],date[2],date[3])
+    print (UGRD[0],UGRD[1],VGRD[0],VGRD[1])
+
 main()
+
+
+#1:0:d=2018020418:UGRD:80 m above ground:anl::lon=30.000000,lat=36.000000,val=-6.21079\n2:3067:d=2018020418:VGRD:80 m above ground:anl::lon=30.000000,lat=36.000000,val=0.509568\n3:6134:d=2018020418:UGRD:100 m above ground:anl::lon=30.000000,lat=36.000000,val=-6.25109\n4:9201:d=2018020418:VGRD:100 m above ground:anl::lon=30.000000,lat=36.000000,val=0.5323\n'
