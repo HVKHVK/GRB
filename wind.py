3e1cbf9b2bfc9f85d4bb772515bc0489dddc56f6
HVKHVK <vkasikaralar@gmail.com>
2018-02-12 14:24:49 +0300

    GRB

--- wind.py
+++ wind.py
@@ -2,6 +2,7 @@ import sys
 import time
 import os
 import subprocess, shlex
+import argparse
 
 
 def main():
@@ -9,9 +10,10 @@ def main():
     lan = sys.argv[2]
     lon = sys.argv[3]
     meter = sys.argv[4]
-    #command_line = input()
-    #args = shlex.split(command_line)
-    #print(args);
     p = subprocess.Popen(['/usr/bin/grib2/wgrib2/wgrib2', filename, '-s', '-lon', lan, lon ])
+    print(p);
+    #parser = argparse.ArgumentParser()
+    #parser.parse_args()
+
 
 main()
