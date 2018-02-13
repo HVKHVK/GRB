ce64d854a27abb7effc9198dd4d2422ac0261dd3
HVKHVK <vkasikaralar@gmail.com>
2018-02-13 09:00:48 +0300

    GRB

--- wind.py
+++ wind.py
@@ -53,7 +53,8 @@ def main():
 
         for i in range(0,len(UGRD)):
             print(date[i*2] + ' ' + coordinates + ' ' + meter[i*2] + ' ' + str(direction[i]) + ' ' + str(speed[i]) + '\n')
-            spamwriter.writerow([date[i*2]]+[coordinates]+[str(meter[i*2])]+[str(direction[i])]+[str(speed[i])])
+            output = date[i*2],coordinates,meter[i*2],str(direction[i]),str(speed[i])
+            spamwriter.writerow(output) 
 
 main()
 
